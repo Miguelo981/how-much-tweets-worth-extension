07854e84484bd7e393eef62d226b46509fe17158
Miguelo981 <55580212+Miguelo981@users.noreply.github.com>
2023-08-16 15:51:00 +0200

    Fix earnings css alignment

--- src/scripting/content.ts
+++ src/scripting/content.ts
@@ -48,11 +48,12 @@ import { getNumberFromString, getParsedPrice } from "../util/parser";
       priceTextSpan.className = span2.className;
       priceTextSpan.textContent = getParsedPrice(tweetPrice);
       priceTextSpan.style.color = "rgb(83, 100, 113)";
-      priceTextSpan.style.marginBottom = "2.5px";
       priceTextSpan.style.fontSize = "13px";
       priceTextSpan.style.fontFamily = `"TwitterChirp",-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Helvetica,Arial,sans-serif`;
 
       const iconContainer = document.createElement("div");
+      iconContainer.style.display = "flex";
+      iconContainer.style.alignItems = "center";
       iconContainer.innerHTML = `<svg style="height: 1.25em" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="rgb(83, 100, 113)" fill="none" stroke-linecap="round" stroke-linejoin="round">
       <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
       <path d="M16.7 8a3 3 0 0 0 -2.7 -2h-4a3 3 0 0 0 0 6h4a3 3 0 0 1 0 6h-4a3 3 0 0 1 -2.7 -2"></path>
