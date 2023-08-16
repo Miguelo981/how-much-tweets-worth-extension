d67067bfabf98af8211985075ae9dbb355e27b92
Miguelo981 <55580212+Miguelo981@users.noreply.github.com>
2023-08-16 15:51:15 +0200

    Fix aria-label selector

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
 export const PRICE_PER_IMPRESSION_IN_DOLLARS = 0.0000085;
-export const IMPRESSION_ARIA_LABEL = "Tweet";
+export const IMPRESSION_ARIA_LABEL = "post";
 export const TWEET_IMPRESSION_INDEX = 3;
